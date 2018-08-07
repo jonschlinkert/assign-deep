a803b7878028de72e5d2665feb93bc30771e6a2d
jonschlinkert <jon.schlinkert@sellside.com>
2018-08-07 13:41:40 -0400

    refactor

--- index.js
+++ index.js
@@ -1,75 +1,37 @@
 /*!
  * assign-deep <https://github.com/jonschlinkert/assign-deep>
  *
- * Copyright (c) 2017, Jon Schlinkert.
+ * Copyright (c) 2017-present, Jon Schlinkert.
  * Released under the MIT License.
  */
 
 'use strict';
 
-var isPrimitive = require('is-primitive');
-var assignSymbols = require('assign-symbols');
-var typeOf = require('kind-of');
-
-function assign(target/*, objects*/) {
-  target = target || {};
-  var len = arguments.length, i = 0;
-  if (len === 1) {
-    return target;
-  }
-  while (++i < len) {
-    var val = arguments[i];
-    if (isPrimitive(target)) {
-      target = val;
-    }
-    if (isObject(val)) {
-      extend(target, val);
-    }
-  }
-  return target;
-}
-
-/**
- * Shallow extend
- */
-
-function extend(target, obj) {
-  assignSymbols(target, obj);
-
-  for (var key in obj) {
-    if (key !== '__proto__' && hasOwn(obj, key)) {
-      var val = obj[key];
-      if (isObject(val)) {
-        if (typeOf(target[key]) === 'undefined' && typeOf(val) === 'function') {
-          target[key] = val;
+const assignSymbols = require('assign-symbols');
+const toString = Object.prototype.toString;
+const assign = module.exports = (target, ...args) => {
+  let i = 0;
+  if (isPrimitive(target)) target = args[i++];
+  if (!target) target = {};
+  for (; i < args.length; i++) {
+    if (isObject(args[i])) {
+      for (const key of Object.keys(args[i])) {
+        if (isObject(target[key]) && isObject(args[i][key])) {
+          assign(target[key], args[i][key]);
+        } else {
+          target[key] = args[i][key];
         }
-        target[key] = assign(target[key] || {}, val);
-      } else {
-        target[key] = val;
       }
+      assignSymbols(target, args[i]);
     }
   }
   return target;
+};
+
+function isObject(val) {
+  return typeof val === 'function' || toString.call(val) === '[object Object]';
 }
 
-/**
- * Returns true if the object is a plain object or a function.
- */
-
-function isObject(obj) {
-  return typeOf(obj) === 'object' || typeOf(obj) === 'function';
+function isPrimitive(val) {
+  return typeof val === 'object' ? val === null : typeof val !== 'function';
 }
-
-/**
- * Returns true if the given `key` is an own property of `obj`.
- */
-
-function hasOwn(obj, key) {
-  return Object.prototype.hasOwnProperty.call(obj, key);
-}
-
-/**
- * Expose `assign`
- */
-
-module.exports = assign;
--- test.js
+++ test.js
@@ -1,165 +1,227 @@
 'use strict';
 
 require('mocha');
-var assert = require('assert');
-var assign = require('./');
+const assert = require('assert');
+const assign = require('./');
 
-describe('assign', function() {
-  it('should deeply assign properties of objects to the first object:', function() {
-    var one = {b: {c: {d: 'e'}}};
-    var two = {b: {c: {f: 'g', j: 'i'}}};
-    assert.deepEqual(assign(one, two), {b: {c: {d: 'e', f: 'g', j: 'i'}}});
+describe('assign', () => {
+  it('should deeply assign properties of additional objects to the first object', () => {
+    let one = { b: { c: { d: 'e' } } };
+    let two = { b: { c: { f: 'g', j: 'i' } } };
+    assert.deepEqual(assign(one, two), { b: { c: { d: 'e', f: 'g', j: 'i' } } });
   });
 
-  it('should extend properties onto a function:', function() {
+  it('should not assign values in arrays (same as Object.assign)', () => {
+    let abc = ['a', 'b'];
+    let xyz = ['c', 'd'];
+    assert(assign(abc, xyz), xyz);
+
+    let obj = { b: [{ a: { a: 0 } }] };
+    let one = assign({}, obj);
+    let two = assign({}, obj);
+
+    assert(one !== obj);
+    assert(two !== obj);
+    assert(one !== two);
+    assert(one.b === two.b);
+    one.b[0].a = { b: 0 };
+    two.b[0].a = { c: 0 };
+    assert.deepEqual(one.b[0].a, { c: 0 });
+  });
+
+  it('should extend properties onto a function', () => {
     function target() {}
-    var one = {b: {c: {d: 'e'}}};
-    var two = {b: {c: {f: 'g', j: 'i'}}};
+    let one = { b: { c: { d: 'e' } } };
+    let two = { b: { c: { f: 'g', j: 'i' } } };
     assign(target, one, two);
-    assert.deepEqual(target.b, {c: {d: 'e', f: 'g', j: 'i'}});
+    assert.deepEqual(target.b, { c: { d: 'e', f: 'g', j: 'i' } });
   });
 
-  it('should extend deeply nested functions:', function() {
-    var fn = function() {};
-    var target = {};
-    var one = {b: {c: {d: fn}}};
-    var two = {b: {c: {f: 'g', j: 'i'}}};
-    assign(target, one, two);
-    assert.deepEqual(target.b, {c: {d: fn, f: 'g', j: 'i'}});
+  it('should extend ignore primitives', () => {
+    function target() {}
+    let one = { b: { c: { d: 'e' } } };
+    let two = { b: { c: { f: 'g', j: 'i' } } };
+    assign('foo', target, one, two);
+    assert.deepEqual(target.b, { c: { d: 'e', f: 'g', j: 'i' } });
   });
 
-  it('should extend deeply nested functions with properties:', function() {
-    var fn = function() {};
+  it('should extend deeply nested functions', () => {
+    let fn = function() {};
+    let target = {};
+    let one = { b: { c: { d: fn } } };
+    let two = { b: { c: { f: 'g', j: 'i' } } };
+    assign(target, one, two);
+    assert.deepEqual(target.b, { c: { d: fn, f: 'g', j: 'i' } });
+  });
+
+  it('should extend deeply nested functions with properties', () => {
+    let fn = function() {};
     fn.foo = 'foo';
     fn.bar = 'bar';
 
-    var target = {};
-    var one = {b: {c: {d: fn}}};
-    var two = {b: {c: {f: 'g', j: 'i'}}};
+    let target = {};
+    let one = { b: { c: { d: fn } } };
+    let two = { b: { c: { f: 'g', j: 'i' } } };
     assign(target, one, two);
-    assert.deepEqual(target.b, {c: {d: fn, f: 'g', j: 'i'}});
+    assert.deepEqual(target.b, { c: { d: fn, f: 'g', j: 'i' } });
   });
 
-  it('should extend deeply nested functions with nested properties:', function() {
-    var fnOne = function() {};
-    fnOne.foo = {y: 'y'};
-    fnOne.bar = {z: 'z'};
-
-    var fnTwo = function() {};
-    fnTwo.foo = {w: 'w'};
-    fnTwo.bar = {x: 'x'};
-
-    var target = {};
-    var one = {a: {fn: fnOne}};
-    var two = {a: {fn: fnTwo}};
-
-    var actual = assign(target, one, two).a.fn;
-    assert.deepEqual(actual.foo, {y: 'y', w: 'w' });
-    assert.deepEqual(actual.bar, { z: 'z', x: 'x' });
+  it('should invoke getters and setters', () => {
+    let name, username;
+    let config = {
+      set name(val) {
+        name = val;
+      },
+      get name() {
+        return name;
+      },
+      set username(val) {
+        username = val;
+      },
+      get username() {
+        return username || 'jonschlinkert';
+      },
+      get other() {
+        return 'other';
+      }
+    };
+    let locals = {
+      get name() {
+        return 'Brian';
+      },
+      get username() {
+        return 'doowb';
+      }
+    }
+    let result = assign(config, locals);
+    assert(result === config);
+    assert.equal(result.name, 'Brian');
+    assert.equal(result.username, 'doowb');
+    assert.equal(result.other, 'other');
   });
 
-  it('should extend properties from functions to functions:', function() {
+  it('should extend functions with nested properties', () => {
+    let aaa = () => {};
+    aaa.foo = { y: 'y' };
+    aaa.bar = { z: 'z' };
+
+    let bbb = () => {};
+    bbb.foo = { w: 'w' };
+    bbb.bar = { x: 'x' };
+
+    let result = assign(aaa, bbb);
+    assert(aaa === result);
+
+    assert.deepEqual(result.foo, { y: 'y', w: 'w' });
+    assert.deepEqual(result.bar, { z: 'z', x: 'x' });
+  });
+
+  it('should extend properties from functions to functions', () => {
     function target() {}
     function one() {}
     function two() {}
-    one.b = {c: {d: 'e'}};
-    two.b = {c: {f: 'g', j: 'i'}};
+    one.b = { c: { d: 'e' } };
+    two.b = { c: { f: 'g', j: 'i' } };
     assign(target, one, two);
-    assert.deepEqual(target.b, {c: {d: 'e', f: 'g', j: 'i'}});
+    assert.deepEqual(target.b, { c: { d: 'e', f: 'g', j: 'i' } });
   });
 
-  it('should update a value when a duplicate is assigned:', function() {
-    var one = {b: {c: {d: 'e'}}};
-    var two = {b: {c: {d: 'f'}}};
-    assert.deepEqual(assign(one, two), {b: {c: {d: 'f'}}});
+  it('should update a value when a duplicate is assigned', () => {
+    let one = { b: { c: { d: 'e' } } };
+    let two = { b: { c: { d: 'f' } } };
+    assert.deepEqual(assign(one, two), { b: { c: { d: 'f' } } });
   });
 
-  it('should not loop over arrays:', function() {
-    var one = {b: {c: {d: 'e', g: ['b']}}};
-    var two = {b: {c: {d: 'f', g: ['a']}}};
-    assert.deepEqual(assign(one, two), {b: {c: {d: 'f', g: ['a']}}});
+  it('should not loop over arrays', () => {
+    let one = { b: { c: { d: 'e', g: ['b'] } } };
+    let two = { b: { c: { d: 'f', g: ['a'] } } };
+    assert.deepEqual(assign(one, two), { b: { c: { d: 'f', g: ['a'] } } });
   });
 
-  it('should deeply assign values from multiple objects:', function() {
-    var foo = {};
-    var bar = {a: 'b'};
-    var baz = {c: 'd', g: {h: 'i'}};
-    var quux = {e: 'f', g: {j: 'k'}};
+  it('should deeply assign values from multiple objects', () => {
+    let foo = {};
+    let bar = { a: 'b' };
+    let baz = { c: 'd', g: { h: 'i' } };
+    let quux = { e: 'f', g: { j: 'k' } };
 
     assign(foo, bar, baz, quux);
-    assert.deepEqual(foo, {a: 'b', c: 'd', e: 'f', g: {h: 'i', j: 'k'}});
+    assert.deepEqual(foo, { a: 'b', c: 'd', e: 'f', g: { h: 'i', j: 'k' } });
   });
 
-  it('should not assign primitive arguments:', function() {
-    var one = {b: {c: {d: 'e', g: ['b']}}};
-    var two = 5;
+  it('should not assign primitive arguments', () => {
+    let one = { b: { c: { d: 'e', g: ['b'] } } };
+    let two = 5;
     assert.deepEqual(assign(one, two), one);
   });
 
-  it('should assign primitive values:', function() {
-    var one = {b: {c: {d: 'e', g: ['b']}}};
-    var two = {b: 5};
-    assert.deepEqual(assign(one, two), {b: 5});
+  it('should assign primitive values', () => {
+    let one = { b: { c: { d: 'e', g: ['b'] } } };
+    let two = { b: 5 };
+    assert.deepEqual(assign(one, two), { b: 5 });
   });
 
-  it('should assign over primitive values:', function() {
-    var one = {b: {c: {d: 'e', g: ['b']}}};
-    var two = {b: 5};
-    var three = {b: function() {}};
+  it('should assign over primitive values', () => {
+    let one = { b: { c: { d: 'e', g: ['b'] } } };
+    let two = { b: 5 };
+    let three = { b: function() {} };
     three.b.foo = 'bar';
     assert.deepEqual(assign(one, two, three), three);
   });
 
-  it('should assign null values:', function() {
-    var one = {b: {c: {d: 'e', g: ['b']}}};
-    var two = {b: null, c: null};
-    assert.deepEqual(assign(one, two), {b: null, c: null});
+  it('should assign null values', () => {
+    let one = { b: { c: { d: 'e', g: ['b'] } } };
+    let two = { b: null, c: null };
+    assert.deepEqual(assign(one, two), { b: null, c: null });
   });
 
-  it('should assign undefined values:', function() {
-    var one = {b: {c: {d: 'e', g: ['b']}}};
-    var two = {b: undefined};
-    assert.deepEqual(assign(one, two), {b: undefined});
+  it('should assign undefined values', () => {
+    let one = { b: { c: { d: 'e', g: ['b'] } } };
+    let two = { b: undefined };
+    assert.deepEqual(assign(one, two), { b: undefined });
   });
 
-  it('should assign properties to a function:', function() {
+  it('should assign properties to a function', () => {
     function one() {}
     one.a = 'b';
     one.c = 'd';
 
-    var two = {e: 'f', g: ['h']};
+    let two = { e: 'f', g: ['h'] };
     assign(one, two);
     assert.deepEqual(one.g, ['h']);
     assert.equal(one.g, two.g);
     assert.equal(typeof one, 'function');
   });
 
-  it('should assign properties from a function:', function() {
+  it('should assign properties from a function', () => {
     function one() {}
     one.a = 'b';
     one.c = 'd';
 
-    var two = {e: 'f', g: ['h']};
+    let two = { e: 'f', g: ['h'] };
     assign(two, one);
     assert.deepEqual(two.g, ['h']);
     assert.equal(two.g, two.g);
     assert.equal(typeof two, 'object');
   });
 
-  it('should deeply mix the properties of object into the first object.', function() {
-    var a = assign({a: {aa: 'aa'} }, {a: {bb: 'bb'} }, {a: {cc: 'cc'} });
-    assert.deepEqual(a, {a: {aa: 'aa', bb: 'bb', cc: 'cc'} });
+  it('should deeply mix the properties of object into the first object.', () => {
+    let a = assign({ a: { aa: 'aa' } }, { a: { bb: 'bb' } }, { a: { cc: 'cc' } });
+    assert.deepEqual(a, { a: { aa: 'aa', bb: 'bb', cc: 'cc' } });
 
-    var b = assign({a: {aa: 'aa', dd: {ee: 'ff'} } }, {a: {bb: 'bb', dd: {gg: 'hh'} } }, {a: {cc: 'cc', dd: {ii: 'jj'} } });
-    assert.deepEqual(b, {a: {aa: 'aa', dd: {ee: 'ff', gg: 'hh', ii: 'jj'}, bb: 'bb', cc: 'cc'} });
+    let b = assign(
+      { a: { aa: 'aa', dd: { ee: 'ff' } } },
+      { a: { bb: 'bb', dd: { gg: 'hh' } } },
+      { a: { cc: 'cc', dd: { ii: 'jj' } } }
+    );
+    assert.deepEqual(b, { a: { aa: 'aa', dd: { ee: 'ff', gg: 'hh', ii: 'jj' }, bb: 'bb', cc: 'cc' } });
   });
 
-  it('should merge object properties without affecting any object', function() {
-    var obj1 = {a: 0, b: 1};
-    var obj2 = {c: 2, d: 3};
-    var obj3 = {a: 4, d: 5};
+  it('should merge object properties without affecting any object', () => {
+    let obj1 = { a: 0, b: 1 };
+    let obj2 = { c: 2, d: 3 };
+    let obj3 = { a: 4, d: 5 };
 
-    var actual = {a: 4, b: 1, c: 2, d: 5 };
+    let actual = { a: 4, b: 1, c: 2, d: 5 };
 
     assert.deepEqual(assign({}, obj1, obj2, obj3), actual);
     assert.notDeepEqual(actual, obj1);
@@ -167,116 +229,116 @@ describe('assign', function() {
     assert.notDeepEqual(actual, obj3);
   });
 
-  it('should do a deep merge', function() {
-    var obj1 = {a: {b: 1, c: 1, d: {e: 1, f: 1}}};
-    var obj2 = {a: {b: 2, d: {f: 'f'}}};
+  it('should do a deep merge', () => {
+    let obj1 = { a: { b: 1, c: 1, d: { e: 1, f: 1 } } };
+    let obj2 = { a: { b: 2, d: { f: 'f' } } };
 
-    assert.deepEqual(assign(obj1, obj2), {a: {b: 2, c: 1, d: {e: 1, f: 'f'} }});
+    assert.deepEqual(assign(obj1, obj2), { a: { b: 2, c: 1, d: { e: 1, f: 'f' } } });
   });
 
-  it('should use the last value defined', function() {
-    var obj1 = {a: 'b'};
-    var obj2 = {a: 'c'};
+  it('should use the last value defined', () => {
+    let obj1 = { a: 'b' };
+    let obj2 = { a: 'c' };
 
-    assert.deepEqual(assign(obj1, obj2), {a: 'c'});
+    assert.deepEqual(assign(obj1, obj2), { a: 'c' });
   });
 
-  it('should use the last value defined on nested object', function() {
-    var obj1 = {a: 'b', c: {d: 'e'}};
-    var obj2 = {a: 'c', c: {d: 'f'}};
+  it('should use the last value defined on nested object', () => {
+    let obj1 = { a: 'b', c: { d: 'e' } };
+    let obj2 = { a: 'c', c: { d: 'f' } };
 
-    assert.deepEqual(assign(obj1, obj2), {a: 'c', c: {d: 'f'}});
+    assert.deepEqual(assign(obj1, obj2), { a: 'c', c: { d: 'f' } });
   });
 
-  it('should shallow clone when an empty object is passed', function() {
-    var obj1 = {a: 'b', c: {d: 'e'}};
-    var obj2 = {a: 'c', c: {d: 'f'}};
+  it('should shallow clone when an empty object is passed', () => {
+    let obj1 = { a: 'b', c: { d: 'e' } };
+    let obj2 = { a: 'c', c: { d: 'f' } };
 
-    var res = assign({}, obj1, obj2);
-    assert.deepEqual(res, {a: 'c', c: {d: 'f'}});
+    let res = assign({}, obj1, obj2);
+    assert.deepEqual(res, { a: 'c', c: { d: 'f' } });
   });
 
-  it('should merge additional objects into the first:', function() {
-    var obj1 = {a: {b: 1, c: 1, d: {e: 1, f: 1}}};
-    var obj2 = {a: {b: 2, d: {f: 'f'} }};
+  it('should merge additional objects into the first', () => {
+    let obj1 = { a: { b: 1, c: 1, d: { e: 1, f: 1 } } };
+    let obj2 = { a: { b: 2, d: { f: 'f' } } };
 
     assign(obj1, obj2);
-    assert.deepEqual(obj1, {a: {b: 2, c: 1, d: {e: 1, f: 'f'} }});
+    assert.deepEqual(obj1, { a: { b: 2, c: 1, d: { e: 1, f: 'f' } } });
   });
 
-  it('should clone objects during merge', function() {
-    var obj1 = {a: {b: 1}};
-    var obj2 = {a: {c: 2}};
+  it('should clone objects during merge', () => {
+    let obj1 = { a: { b: 1 } };
+    let obj2 = { a: { c: 2 } };
 
-    var target = assign({}, obj1, obj2);
-    assert.deepEqual(target, {a: {b: 1, c: 2}});
-    assert.deepEqual(target.a, {b: 1, c: 2});
+    let target = assign({}, obj1, obj2);
+    assert.deepEqual(target, { a: { b: 1, c: 2 } });
+    assert.deepEqual(target.a, { b: 1, c: 2 });
   });
 
-  it('should deep clone arrays during merge', function() {
-    var obj1 = {a: [1, 2, [3, 4]]};
-    var obj2 = {b: [5, 6]};
+  it('should deep clone arrays during merge', () => {
+    let obj1 = { a: [1, 2, [3, 4]] };
+    let obj2 = { b: [5, 6] };
 
-    var actual = assign(obj1, obj2);
+    let actual = assign(obj1, obj2);
     assert.deepEqual(actual.a, [1, 2, [3, 4]]);
     assert.deepEqual(actual.a[2], [3, 4]);
     assert.deepEqual(actual.b, obj2.b);
   });
 
-  it('should copy source properties', function() {
+  it('should copy source properties', () => {
     assert(assign({ test: true }).test, true);
   });
 
-  it('should not deep clone arrays', function() {
+  it('should not deep clone arrays', () => {
     assert.deepEqual(assign([1, 2, 3]), [1, 2, 3]);
     assert.deepEqual(assign([1, 2, 3], {}), [1, 2, 3]);
   });
 
-  it('should work with sparse objects:', function() {
-    var actual = assign({}, undefined, {a: 'b'}, undefined, {c: 'd'});
-    assert.deepEqual(actual, {a: 'b', c: 'd'});
+  it('should iterate over sparse arguments', () => {
+    let actual = assign({}, undefined, { a: 'b' }, undefined, { c: 'd' });
+    assert.deepEqual(actual, { a: 'b', c: 'd' });
   });
 
-  it('should clone RegExps', function() {
-    var fixture = /test/g;
-    var actual = assign(fixture);
+  it('should clone RegExps', () => {
+    let fixture = /test/g;
+    let actual = assign(fixture);
     assert.deepEqual(actual, fixture);
   });
 
-  it('should clone Dates', function() {
-    var fixture = new Date();
-    var actual = assign(fixture);
+  it('should clone Dates', () => {
+    let fixture = new Date();
+    let actual = assign(fixture);
     assert.deepEqual(actual, fixture);
   });
 
-  it('should not clone objects created with custom constructor', function() {
-    function TestType() { }
-    var fixture = new TestType();
-    var actual = assign(fixture);
+  it('should not clone objects created with custom constructor', () => {
+    function TestType() {}
+    let fixture = new TestType();
+    let actual = assign(fixture);
     assert.deepEqual(actual, fixture);
   });
 });
 
-describe('symbols', function() {
-  it('should return the first object when one argument is passed:', function() {
-    assert.deepEqual(assign({a: 'b'}), {a: 'b'});
+describe('symbols', () => {
+  it('should return the first object when one argument is passed', () => {
+    assert.deepEqual(assign({ a: 'b' }), { a: 'b' });
   });
 
   if (typeof Symbol !== 'undefined') {
-    it('should assign symbol properties from an object to the receiver', function() {
-      var a = {};
-      var b = {};
-      var key = Symbol('abc');
+    it('should assign symbol properties from an object to the receiver', () => {
+      let a = {};
+      let b = {};
+      let key = Symbol('abc');
       b[key] = 'xyz';
       assign(a, b);
       assert.equal(a[key], 'xyz');
     });
 
-    it('should deeply assign symbol properties', function() {
-      var a = {c: {e: {f: {}}}};
-      var b = {c: {e: {g: {}}}};
-      var foo = Symbol('foo');
-      var bar = Symbol('bar');
+    it('should deeply assign symbol properties', () => {
+      let a = { c: { e: { f: {} } } };
+      let b = { c: { e: { g: {} } } };
+      let foo = Symbol('foo');
+      let bar = Symbol('bar');
       a.c.e.f[foo] = 'xyz';
       b.c.e.g[bar] = 'xyz';
       assign(a, b);
@@ -284,18 +346,18 @@ describe('symbols', function() {
       assert.equal(a.c.e.g[bar], 'xyz');
     });
 
-    it('should assign symbol properties from each object to the receiver', function() {
-      var target = {};
-      var a = {};
-      var aa = Symbol('aa');
+    it('should assign symbol properties from each object to the receiver', () => {
+      let target = {};
+      let a = {};
+      let aa = Symbol('aa');
       a[aa] = 'aa';
 
-      var b = {};
-      var bb = Symbol('bb');
+      let b = {};
+      let bb = Symbol('bb');
       b[bb] = 'bb';
 
-      var c = {};
-      var cc = Symbol('cc');
+      let c = {};
+      let cc = Symbol('cc');
       c[cc] = 'cc';
 
       assign(target, a, b, c);
@@ -304,22 +366,22 @@ describe('symbols', function() {
       assert.equal(target[cc], 'cc');
     });
 
-    it('should not assign non-enumerable symbols', function() {
-      var a = {};
-      var key = Symbol('abc');
+    it('should not assign non-enumerable symbols', () => {
+      let a = {};
+      let key = Symbol('abc');
       function App() {}
       App.prototype[key] = 'xyz';
-      var app = new App();
+      let app = new App();
       assign(a, app);
       assert.equal(typeof a[key], 'undefined');
     });
 
-    it('should return the receiver object', function() {
-      var a = {};
-      var b = {};
-      var key = Symbol('abc');
+    it('should return the receiver object', () => {
+      let a = {};
+      let b = {};
+      let key = Symbol('abc');
       b[key] = 'xyz';
-      var res = assign(a, b);
+      let res = assign(a, b);
       assert.equal(res[key], 'xyz');
     });
   }
